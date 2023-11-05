aacdd59665a98e972a1c264fda762a5271fe0308
Given <givenchyampulembang01@gmail.com>
2023-11-05 16:55:18 +0900

    WIP:
       - Change The Apps For Portrait Only
       - add assets at pubspec.yaml

--- lib/presentation/widget/app_input_text.dart
+++ lib/presentation/widget/app_input_text.dart
@@ -1,5 +1,4 @@
 import 'package:booking_app/config/config.dart';
-import 'package:booking_app/config/theme/app_dimen.dart';
 import 'package:flutter/material.dart';
 
 class AppInputText extends StatefulWidget {
