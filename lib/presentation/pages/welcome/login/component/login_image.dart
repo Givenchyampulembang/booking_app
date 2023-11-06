4ab2a23f95604025e0cd374d171aecfdff0be49f
Given <givenchyampulembang01@gmail.com>
2023-11-06 23:43:00 +0900

    WIP:
       - Change app_input_text.dart to StatelessWidget
       - Change login_image.dart to StatefulWidget

--- lib/presentation/pages/welcome/login/component/login_image.dart
+++ lib/presentation/pages/welcome/login/component/login_image.dart
@@ -1,9 +1,14 @@
 import 'package:booking_app/data/src/img_string.dart';
 import 'package:flutter/material.dart';
 
-class LoginImage extends StatelessWidget {
+class LoginImage extends StatefulWidget {
   const LoginImage({super.key});
 
+  @override
+  State<LoginImage> createState() => _LoginImageState();
+}
+
+class _LoginImageState extends State<LoginImage> {
   @override
   Widget build(BuildContext context) {
     return Image.asset(ImgString.cittaPlants2);
