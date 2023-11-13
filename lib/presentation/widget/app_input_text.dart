d23deb36b19ee26bed5d6b3896deda34f8a09ef1
Given <givenchyampulembang01@gmail.com>
2023-11-13 22:43:43 +0900

    WIP:
       - Change app_input_text.dart to StatelessWidget

--- lib/presentation/widget/app_input_text.dart
+++ lib/presentation/widget/app_input_text.dart
@@ -1,21 +1,25 @@
 import 'package:booking_app/config/config.dart';
 import 'package:flutter/material.dart';
 
-class AppInputText extends StatelessWidget {
+class AppInputText extends StatefulWidget {
+  final TextEditingController? controller;
+  final String label;
   const AppInputText({Key? key, this.controller, required this.label})
       : super(key: key);
 
-  final TextEditingController? controller;
-  final String label;
+  @override
+  State<AppInputText> createState() => _AppInputTextState();
+}
 
+class _AppInputTextState extends State<AppInputText> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: AppDimen.h40,
       child: TextFormField(
-        controller: controller,
+        controller: widget.controller,
         decoration: InputDecoration(
-          hintText: label,
+          hintText: widget.label,
           hintStyle: AppFont.componentMedium,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(4),
